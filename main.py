3643781f138e7945d49ed342636e788eb61326b3
Madrianoliko <adrianmalik333@gmail.com>
2023-02-23 16:56:57 +0100

    lagos test

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 from map_calculate import calculate, calculateForVideoOnlySeq
 
-calculate(1,'./samples/uavNavigationTurning2.mp4','./samples/sectorMapSample2.png')
+calculate(1,'./samples/lagosUAV.mp4','./samples/sectorMapSample3.png')
 # calculateForVideoOnlySeq('./samples/uavNavigationTurning2.mp4','./samples/sectorMapSample2.png',1154,367)
